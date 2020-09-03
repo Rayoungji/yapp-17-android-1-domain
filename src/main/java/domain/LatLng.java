ef47304b341691061d53effc0afa3c019355ede8
rayoungji <dudwl7676@naver.com>
2020-09-03 16:08:38 +0900

    LatLngTest 테스트 작성

--- src/main/java/domain/LatLng.java
+++ src/main/java/domain/LatLng.java
@@ -5,6 +5,10 @@ public class LatLng {
     private double lng;
 
     public LatLng(double lat, double lng) {
+        int STANDARDLAT = 7;
+        validateLatLng(lat, STANDARDLAT);
+        int STANDARDLNG = 6;
+        validateLatLng(lng, STANDARDLNG);
         this.lat = lat;
         this.lng = lng;
     }
@@ -15,5 +19,17 @@ public class LatLng {
 
     public double getLng() {
         return  lng;
+    }
+
+    public void validateLatLng(double latLng, int standard){
+        String split[] = Double.toString(latLng).split("\\.");
+        int latLngSize = split[1].length();
+
+        if(standard==7 && latLngSize!=7){
+            throw new IllegalArgumentException("위도는 7자리여야 합니다");
+        }
+        if(standard==6 && latLngSize!=6){
+            throw new IllegalArgumentException("경도는 6자리여야 합니다");
+        }
     }
 }
--- src/test/java/domain/LatLngTest.java
+++ src/test/java/domain/LatLngTest.java
@@ -2,27 +2,40 @@ package domain;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
 
 class LatLngTest {
 
-    @Test
+    @ParameterizedTest
+    @ValueSource(doubles = {1234.1234,12.19385746})
     @DisplayName("위도가 7자리가 아닐 시 예외처리")
-    void testCreateLatFail() throws Exception {
+    void testCreateLatFail(double lat) {
 
+        //예외처리 테스트 - 첫번째 인자(나와야 할 예외처리에 대한 클래스), 두번째 인자(실행 클래스)
+        assertThrows(IllegalArgumentException.class, () -> new LatLng(lat,12.123456));
     }
 
-    @Test
+    @ParameterizedTest
+    @ValueSource(doubles = {1234.1234,12.1234567})
     @DisplayName("경도가 6자리가 아닐 시 예외처리")
-    void testCreateLngFail() throws Exception {
+    void testCreateLngFail(double lng) {
 
+        assertThrows(IllegalArgumentException.class,() -> new LatLng(12.1234567,lng));
     }
 
     @Test
     @DisplayName("좌표 생성")
-    void testCreateLatLngSuccess() throws Exception {
+    void testCreateLatLngSuccess() {
+        //when
+        LatLng latLng = new LatLng(0.1234567,12.123456);
 
+        //then
+        assertThat(latLng).isNotNull();
     }
 
 }
